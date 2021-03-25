2006dce61d2bb0c2223c288b09766a9fe93a4315
Neeraj Solanki <neeraj.solanki@jeevansathi.com>
2021-03-25 22:06:23 +0530

    Initial Commit

---
+++ TrueCaller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrueCaller
+//
+//  Created by Neeraj Solanki on 25/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
